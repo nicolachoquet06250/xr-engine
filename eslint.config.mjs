7380a2a308216e74ebffcbeae83930851063d238
nchoquet <nicolachoquet06250@gmail.com>
2026-04-22 11:03:02 +0200

    fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,23 @@
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  ...tseslint.configs.recommended,
-  {
-    files: ['**/*.ts', '**/*.tsx', '**/*.d.ts'],
-    languageOptions: {
-      parserOptions: {
-        projectService: false,
-        tsconfigRootDir: import.meta.dirname
-      },
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
+export default tseslint.config(...tseslint.configs.recommended, {
+  files: ['**/*.ts', '**/*.tsx', '**/*.d.ts'],
+  languageOptions: {
+    parserOptions: {
+      projectService: false,
+      tsconfigRootDir: import.meta.dirname,
     },
-    rules: {
-      '@typescript-eslint/consistent-type-imports': 'error',
-      '@typescript-eslint/no-unused-vars': 'off',//['error', { argsIgnorePattern: '^_' }],
-      '@typescript-eslint/no-empty-interface': 'off',
-      '@typescript-eslint/no-empty-object-type': 'off'
+    globals: {
+      ...globals.browser,
+      ...globals.node,
     },
-    ignores: ['**/dist/**', '**/coverage/**', '**/node_modules/**'],
-  }
-);
+  },
+  rules: {
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/no-unused-vars': 'off', //['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-empty-object-type': 'off',
+  },
+  ignores: ['**/dist/**', '**/coverage/**', '**/node_modules/**'],
+});
