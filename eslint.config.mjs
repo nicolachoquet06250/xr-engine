7a17cba73a34092088ee0d16a3ada3f9b057c2a8
nchoquet <nicolachoquet06250@gmail.com>
2026-04-23 12:12:15 +0200

    fix eslint conf

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,22 +7,23 @@ export default tseslint.config(
   },
   ...tseslint.configs.recommended,
   {
-  files: ['**/*.ts', '**/*.tsx', '**/*.d.ts'],
-  languageOptions: {
-    parserOptions: {
-      projectService: false,
-      tsconfigRootDir: import.meta.dirname,
+    files: ['**/*.ts', '**/*.tsx', '**/*.d.ts'],
+    languageOptions: {
+      parserOptions: {
+        projectService: false,
+        tsconfigRootDir: import.meta.dirname,
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
-    globals: {
-      ...globals.browser,
-      ...globals.node,
+    rules: {
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/no-unused-vars': 'off', //['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-empty-interface': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/no-this-alias': 'off',
     },
-  },
-  rules: {
-    '@typescript-eslint/consistent-type-imports': 'error',
-    '@typescript-eslint/no-unused-vars': 'off', //['error', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/no-empty-interface': 'off',
-    '@typescript-eslint/no-empty-object-type': 'off',
-    '@typescript-eslint/no-this-alias': 'off',
   }
-});
+);
